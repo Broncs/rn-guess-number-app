1149141fd89320639ee1d6d9107fc634ca4992f8
Jhonatan mendes <jhowfrombrazil@gmail.com>
2021-06-17 12:13:06 -0300

    fix adptive landscape on game over screen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Button,
   StyleSheet,
@@ -14,11 +14,41 @@ import TitleText from "../components/TitleText";
 import Colors from "../constants/colors";
 
 const GameOverScreen = ({ rounds, userNumber, onRestart }) => {
+  const [availableDeviceWidth, setAvailableDeviceWidth] = useState(
+    Dimensions.get("window").width
+  );
+  const [availableDeviceHeight, setAvailableDeviceHeight] = useState(
+    Dimensions.get("window").height
+  );
+
+  useEffect(() => {
+    const updateImgSize = () => {
+      setAvailableDeviceHeight(Dimensions.get("window").height);
+      setAvailableDeviceWidth(Dimensions.get("window").width);
+    };
+
+    Dimensions.addEventListener("change", updateImgSize);
+
+    return () => {
+      Dimensions.removeEventListener("change", updateImgSize);
+    };
+  });
+
   return (
     <ScrollView>
       <View style={styles.screen}>
         <TitleText>O jogo acabou!</TitleText>
-        <View style={styles.imageContainer}>
+        <View
+          style={{
+            ...styles.imageContainer,
+            ...{
+              width: availableDeviceWidth * 0.7,
+              height: availableDeviceWidth * 0.7,
+              borderRadius: (availableDeviceWidth * 0.7) / 2,
+              marginVertical: availableDeviceWidth / 30,
+            },
+          }}
+        >
           <Image
             source={require("../assets/success.png")}
             // source={{
@@ -30,8 +60,20 @@ const GameOverScreen = ({ rounds, userNumber, onRestart }) => {
           />
         </View>
 
-        <View style={styles.resultContainer}>
-          <BodyText style={styles.resultText}>
+        <View
+          style={{
+            ...styles.resultContainer,
+            ...{ marginVertical: availableDeviceHeight / 60 },
+          }}
+        >
+          <BodyText
+            style={{
+              ...styles.resultText,
+              ...{
+                fontSize: availableDeviceHeight < 400 ? 16 : 20,
+              },
+            }}
+          >
             Seu celular precisou de{" "}
             <Text style={styles.highlight}>{rounds}</Text> tentativas para
             acertar o numero <Text style={styles.highlight}>{userNumber}.</Text>
@@ -51,15 +93,13 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    paddingVertical: 10,
   },
   imageContainer: {
-    width: Dimensions.get("window").width * 0.7,
-    height: Dimensions.get("window").width * 0.7,
-    borderRadius: (Dimensions.get("window").width * 0.7) / 2,
     borderWidth: 4,
     borderColor: "black",
     overflow: "hidden",
-    marginVertical: Dimensions.get("window").height / 30, //equals to 5%  ... divide it by 40 and you'll get 2.5%
+    // marginVertical: Dimensions.get("window").height / 30, //equals to 5%  ... divide it by 40 and you'll get 2.5%
   },
   image: {
     width: "100%",
@@ -68,11 +108,9 @@ const styles = StyleSheet.create({
 
   resultContainer: {
     marginHorizontal: 30,
-    marginVertical: Dimensions.get("window").height / 60,
   },
   resultText: {
     textAlign: "center",
-    fontSize: Dimensions.get("window").height < 400 ? 16 : 20,
   },
   highlight: {
     color: Colors.primary,
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -38,9 +38,28 @@ const GameScreen = ({ userChoice, onGameOver }) => {
   const initialGuess = generateRandomBetween(1, 100, userChoice);
   const [currentGuess, setCurrentGuess] = useState(initialGuess);
   const [pastGuesses, setPassGuesses] = useState([initialGuess.toString()]);
+  const [availableDeviceWidth, setAvailableDeviceWidth] = useState(
+    Dimensions.get("window").width
+  );
+
+  const [availableDeviceHeight, setAvailableDeviceHeight] = useState(
+    Dimensions.get("window").height
+  );
 
   const currentLow = useRef(1);
   const currentHigh = useRef(100);
+
+  useEffect(() => {
+    const updateLayout = () => {
+      setAvailableDeviceWidth(Dimensions.get("window").width);
+      setAvailableDeviceHeight(Dimensions.get("window").height);
+    };
+    Dimensions.addEventListener("change", updateLayout);
+
+    return () => {
+      Dimensions.removeEventListener("change", updateLayout);
+    };
+  });
 
   useEffect(() => {
     if (currentGuess === userChoice) {
@@ -81,10 +100,43 @@ const GameScreen = ({ userChoice, onGameOver }) => {
 
   let listContainerStyle = styles.listContainer;
 
-  if (Dimensions.get("window").width < 350) {
+  if (availableDeviceWidth < 350) {
     listContainerStyle = styles.listContainerBigger;
   }
 
+  if (availableDeviceHeight < 500) {
+    return (
+      <View style={styles.screen}>
+        <TitleText>Palpite do oponente</TitleText>
+
+        <View style={styles.controls}>
+          <MainButton onPress={nextGuessHandler.bind(this, "menor")}>
+            <Ionicons name="md-remove" size={24} color="white" />
+          </MainButton>
+
+          <NumberContainer>{currentGuess}</NumberContainer>
+
+          <MainButton onPress={nextGuessHandler.bind(this, "maior")}>
+            <Ionicons name="md-add" size={24} color="white" />
+          </MainButton>
+        </View>
+
+        <View style={listContainerStyle}>
+          {/* <ScrollView contentContainerStyle={styles.list}>
+        {pastGuesses.map((guess, index) =>
+          renderListItem(guess, pastGuesses.length - index)
+        )}
+      </ScrollView> */}
+          <FlatList
+            keyExtractor={(item) => item}
+            data={pastGuesses}
+            renderItem={renderListItem.bind(this, pastGuesses.length)}
+            contentContainerStyle={styles.list}
+          />
+        </View>
+      </View>
+    );
+  }
   return (
     <View style={styles.screen}>
       <TitleText>Palpite do oponente</TitleText>
@@ -128,6 +180,12 @@ const styles = StyleSheet.create({
     marginTop: Dimensions.get("window").height > 600 ? 20 : 5,
     width: 400,
     maxWidth: "80%",
+  },
+  controls: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    width: "80%",
   },
   listContainer: {
     width: "60%",
